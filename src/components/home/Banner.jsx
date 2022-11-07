d310eeed6ba076747ba2a116a7b463524cd55a10
PRATHAM MORE <pratham02more20@gmail.com>
2022-11-07 22:10:16 +0530

    code shifted to main

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel';
-import {bannerData} from '../../constants/data';
-import { makeStyles } from '@material-ui/core';
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { bannerData } from "../../constants/data";
+import { makeStyles } from "@material-ui/core";
 
 const useStyle = makeStyles({
-    image: {
-        width: '100%',
-        height: 300
-
-    },
-    carousel:{
-        marginTop: 20
-    }
-})
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  carousel: {
+    marginTop: 20,
+  },
+});
 
 const Banner = () => {
-    const classes = useStyle();
-    return(
-        <Carousel cycleNavigation={true} className={classes.carousel} >
-            {
-                bannerData.map(image => (
-                    <img src={image.url} className={classes.image} key={image.id} alt="Banner"/>
-                ))
-            }
-        </Carousel>
-    )
-}
+  const classes = useStyle();
+  return (
+    <Carousel cycleNavigation={true} className={classes.carousel}>
+      {bannerData.map((image) => (
+        <img
+          src={image.url}
+          className={classes.image}
+          key={image.id}
+          alt="Banner"
+        />
+      ))}
+    </Carousel>
+  );
+};
 
 export default Banner;
--- src/components/home/NavBar.jsx
+++ src/components/home/NavBar.jsx
@@ -1,55 +1,51 @@
-import React from 'react';
-import { Box, Typography, makeStyles } from '@material-ui/core';
-import { navData } from '../../constants/data';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Box, Typography, makeStyles } from "@material-ui/core";
+import { navData } from "../../constants/data";
+import { Link } from "react-router-dom";
 
 const useStyle = makeStyles({
-    component: {
-        display: 'flex',
-        margin: '20px 130px',
-        background: '#FBF8F1',
-        justifyContent: 'center'
-        // overflow: 'overlay'
-
-    },
-    container: {
-        textAlign: 'center',
-        padding: '12px 8px',
-        justifyContent: 'space-between',
-        textDecoration:'none',
-        color: 'black'
-
-
-    },
-    image: {
-        width: 64
-    },
-    text: {
-        fontSize: 14,
-        fontWeight: 600,
-        textDecoration:'none',
-        color: 'black'
-    }
-
-})
-
+  component: {
+    display: "flex",
+    margin: "20px 130px",
+    background: "#FBF8F1",
+    justifyContent: "center",
+    // overflow: 'overlay'
+  },
+  container: {
+    textAlign: "center",
+    padding: "12px 8px",
+    justifyContent: "space-between",
+    textDecoration: "none",
+    color: "black",
+  },
+  image: {
+    width: 64,
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: 600,
+    textDecoration: "none",
+    color: "black",
+  },
+});
 
 const NavBar = () => {
-    const classes = useStyle();
-    return (
-        <Box className={classes.component}>
-            {
-                navData.map(data => (
-                    <Link to='/product' className={classes.container} key={data.id}>
-                        <img src={data.url} className={classes.image} alt="Navbar" />
-                        <Typography className={classes.text}>{data.text}</Typography>
-                    </Link>
-                ))
-            }
-        </Box>
-
-
-    )
-}
+  const classes = useStyle();
+  return (
+    <Box className={classes.component}>
+      {navData.map((data) => (
+        <Link to="/product" className={classes.container}>
+          <img
+            src={data.url}
+            className={classes.image}
+            key={data.id}
+            alt="Navbar"
+          />
+          <Typography className={classes.text}>{data.text}</Typography>
+        </Link>
+      ))}
+    </Box>
+  );
+};
 
 export default NavBar;
